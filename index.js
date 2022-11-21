5767865b4beb0c9d1593af966bc0a36145b09c8b
r-izume <izume.rikuto@gmail.com>
2022-11-21 11:28:32 +0900

    time_count

--- index.js
+++ index.js
@@ -201,5 +201,7 @@ function Time_Count(){
 }
 
 setInterval(() => {for(let l = 1;l <= 3;l++){
+    if(lock_data.find((c) => c.id === l).lock === 'yes'){
     lock_data.find((c) => c.id === l).time += 10;
+    }
 }},10000);
